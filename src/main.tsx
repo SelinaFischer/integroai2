e1cf4561c1abbbda0f0a836d6417231c0a922cd5
Selina Fischer <selinafishx@gmail.com>
2026-01-12 12:41:03 +0000

    Implement redirection for old blog URLs
    Add URL redirection for old .html blog URLs to SPA URLs.

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+// Redirect old .html blog URLs to clean SPA URLs
+if (window.location.pathname.endsWith('.html')) {
+  const newPath = window.location.pathname.replace('.html', '');
+  window.location.replace(newPath + window.location.search + window.location.hash);
+}
+
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
