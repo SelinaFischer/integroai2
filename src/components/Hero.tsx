23c1489d83f907e8852402a1ae3843921c426046
Selina Fischer <selinafishx@gmail.com>
2026-01-23 03:07:35 +0000

    Add blur effect to bottom wave div

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -121,7 +121,7 @@ const Hero = () => {
       </motion.div>
 
       {/* Bottom wave */}
-      <div className="absolute bottom-0 left-0 w-full opacity-40 sm:opacity-60 pointer-events-none">
+      <div className="absolute bottom-0 left-0 w-full opacity-40 sm:opacity-60 pointer-events-none blur-[0.5px]">
         <svg
           viewBox="0 0 1440 80"
           fill="none"
