eb8c47169fc8fdfcc093b5bf624d17903053b50c
Selina Fischer <selinafishx@gmail.com>
2026-06-11 23:21:34 +0100

    Add files via upload

--- src/components/BlogPosts.tsx
+++ src/components/BlogPosts.tsx
@@ -135,10 +135,12 @@ const BlogPosts = () => {
           transition={{ duration: 0.5, delay: 0.4 }}
           className="text-center mt-12"
         >
-          <Button variant="heroOutline" size="lg" className="group">
-            View All Articles
-            <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-transform" />
-          </Button>
+          <Link to="/blog">
+            <Button variant="heroOutline" size="lg" className="group">
+              View All Articles
+              <ArrowRight className="w-4 h-4 group-hover:translate-x-1 transition-transform" />
+            </Button>
+          </Link>
         </motion.div>
       </div>
     </section>
